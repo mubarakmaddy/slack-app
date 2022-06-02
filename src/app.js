fc16e84aa6f571ee449f61cb1f4de3191544fa8d
mubarakmaddy <42964314+mubarakmaddy@users.noreply.github.com>
2022-06-02 21:55:07 +0530

    removed reciver

--- src/app.js
+++ src/app.js
@@ -7,6 +7,13 @@ const {
   SLACK_REDIRECT_URL,
 } = require('./constants');
 
+const {
+  buildPutWorkspaceParams,
+  buildSlackInstallation,
+} = require('./helpers');
+
+const database = {};
+
 const oauthStateCheck = () => {
   console.log('oauthStateCheck');
 };
@@ -20,22 +27,63 @@ const authorizeFn = () => {
   console.log('authorizeFn');
 };
 
-const receiver = Auth({
+const app = new App({
+  signingSecret: SLACK_SIGNING_SECRET,
   clientId: SLACK_CLIENT_ID,
   clientSecret: SLACK_CLIENT_SECRET,
-  signingSecret: SLACK_SIGNING_SECRET,
-  redirectUrl: SLACK_REDIRECT_URL,
-  stateCheck: oauthStateCheck,
-  onSuccess: oauthSuccess,
-  onError: oauthError,
-  useSlackOauthV2: true,
+  stateSecret: 'my-secret',
+  scopes: ['chat:write', 'commands'],
+  installationStore: {
+    storeInstallation: async (installation) => {
+      // Bolt will pass your handler an installation object
+      // Change the lines below so they save to your database
+      if (
+        installation.isEnterpriseInstall &&
+        installation.enterprise !== undefined
+      ) {
+        // handle storing org-wide app installation
+        return await database.set(installation.enterprise.id, installation);
+      }
+      if (installation.team !== undefined) {
+        // single team app installation
+        return await database.set(installation.team.id, installation);
+      }
+      throw new Error('Failed saving installation data to installationStore');
+    },
+    fetchInstallation: async (installQuery) => {
+      // Bolt will pass your handler an installQuery object
+      // Change the lines below so they fetch from your database
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        // handle org wide app installation lookup
+        return await database.get(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        // single team app installation lookup
+        return await database.get(installQuery.teamId);
+      }
+      throw new Error('Failed fetching installation');
+    },
+    deleteInstallation: async (installQuery) => {
+      // Bolt will pass your handler  an installQuery object
+      // Change the lines below so they delete from your database
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        // org wide app installation deletion
+        return await database.delete(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        // single team app installation deletion
+        return await database.delete(installQuery.teamId);
+      }
+      throw new Error('Failed to delete installation');
+    },
+  },
 });
-
-const app = new App({
-  authorize: authorizeFn,
-  receiver,
-});
-
 // All the room in the world for your code
 app.event('app_home_opened', async ({ event, client, context }) => {
   try {
@@ -122,10 +170,10 @@ app.command('/knowledge', async ({ command, ack, say }) => {
 });
 
 // Set up other handling for other web requests as methods on receiver.router
-receiver.router.get('/redirect-url', (req, res) => {
-  // You're working with an express req and res now.
-  res.send('yay!');
-});
+// receiver.router.get('/redirect-url', (req, res) => {
+//   // You're working with an express req and res now.
+//   res.send('yay!');
+// });
 
 (async () => {
   // Start your app
